018a69494c19d092b308e1f0685da03a8e30cb18
Hronom <hronom@gmail.com>
2017-09-24 00:44:48 +0300

    Update logging

--- supervisor/src/main/java/com/github/hronom/spring/boot/quartz/cluster/example/supervisor/services/TestServiceImpl.java
+++ supervisor/src/main/java/com/github/hronom/spring/boot/quartz/cluster/example/supervisor/services/TestServiceImpl.java
@@ -2,20 +2,23 @@ package com.github.hronom.spring.boot.quartz.cluster.example.supervisor.services
 
 import com.github.hronom.spring.boot.quartz.cluster.example.common.service.TestService;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class TestServiceImpl implements TestService {
+    private final Log logger = LogFactory.getLog(getClass());
 
     public void run(String id) {
-        System.out.println("Running job on supervisor, job id " + id);
+        logger.info("Running job on supervisor, job id " + id);
         try {
             Thread.sleep(TimeUnit.MINUTES.toMillis(1));
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            logger.error("Error", e);
         }
-        System.out.println("Completed job on supervisor, job id " + id);
+        logger.info("Completed job on supervisor, job id " + id);
     }
 }
